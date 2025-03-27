08fd552ce866e4bad7807d246b8a4f17ed2cce60
Antonio Vilchez <antoniovmonge@gmail.com>
2025-03-27 18:58:10 +0100

    upd schema v1

--- app/core/urls.py
+++ app/core/urls.py
@@ -6,9 +6,9 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include("chinook.urls")),
-    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
-        "api/schema/swagger-ui/",
+        "api/v1/schema/swagger-ui/",
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui",
     ),
